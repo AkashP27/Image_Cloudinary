c40caa6270ffd7b20165efe1ecda19a644323d90
AkashP27 <akashphayade27@gmail.com>
2023-07-08 16:46:45 +0530

    providing image services

---
+++ src/main/java/com/imagegallery/service/ImageService.java
@@ -0,0 +1,40 @@
+package com.imagegallery.service;
+
+import com.imagegallery.entity.Image;
+import com.imagegallery.repository.ImageRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ImageService {
+
+    @Autowired
+    ImageRepository imageRepository;
+
+    public List<Image> get(){
+        return imageRepository.findByOrderById();
+    }
+
+    public Optional<Image> getOne(int id){
+        return imageRepository.findById(id);
+    }
+
+    public void save(Image image){
+        imageRepository.save(image);
+    }
+
+    public void delete(int id){
+        imageRepository.deleteById(id);
+    }
+
+    public boolean exists(int id){
+        return imageRepository.existsById(id);
+    }
+}
+
+
